9b9fbd45f63c3cfc9f8c90a13d8056ee91f2f657
wangdesheng <737708900@qq.com>
2019-02-14 01:20:25 +0800

    个人页UI

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,6 +1,8 @@
+import {getStatusBarHeight} from "../../utils/navbar_tool";
 
 Page({
   data: {
+    paddingTop: getStatusBarHeight(),
   },
   onLoad: function () {
   }
--- pages/menu/data.js
+++ pages/menu/data.js
@@ -6,7 +6,7 @@ export const searchKeys = [
 export const hotMenus = [
   {
     id: 1,
-    title: '水煮虾',
+    title: '水煮虾,水煮虾水煮虾水煮虾水煮虾,水煮虾水煮虾水煮虾水煮虾水煮虾水煮虾水煮虾',
     image: 'http://oss.huangye88.net/live/user/2305395/1498122011094096400-0.jpg',
     author: '莉*莉',
     authorIcon: 'http://pic.qqtn.com/up/2017-11/2017110215093888510.jpg',
