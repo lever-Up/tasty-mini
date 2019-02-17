6e78a011f042f2a7965bd7576292a02f4d6919bf
wangdesheng <737708900@qq.com>
2019-02-18 01:11:24 +0800

    完成直采UI(数据加载待优化)

--- pages_news/live_broadcast/data.js
+++ pages_news/live_broadcast/data.js
@@ -9,14 +9,33 @@ export const daily = [
     day: 20,
     dateRange: '2019-5-4~2019-6-8',
     title: '第三期直采,2019年2月17日 16:26:07',
-    image: 'http://pn01lr3h0.bkt.clouddn.com/tvc-v2_30097df.mp4?vframe/jpg/offset/6/w/375/h/667',
+    images: [
+      {
+        id: 1,
+        image: 'http://image.hnol.net/c/2014-03/19/19/20140319191055351-2089977.jpg',
+        desc: 111111111,
+      },
+      {
+        id: 2,
+        image: 'http://i00.c.aliimg.com/img/ibank/2014/703/419/1351914307_1676575415.jpg',
+        desc: 22222222,
+      },
+      {
+        id: 3,
+        image: 'https://tse1.mm.bing.net/th?id=OIP.fKM62APlW8Qdm8CC96UEKQHaL2&pid=Api',
+        desc: 33333333,
+      },
+      {
+        id: 4,
+        image: 'https://tse4.mm.bing.net/th?id=OIP.4L9esW0GzCHCIPBPfDS3WwHaEo&pid=Api',
+        desc: 444444444444,
+      }
+    ],
     messages: [
-      '1111',
-      '22222',
-      '333333',
-      '4444444',
-      '55555555',
-      '666666666',
+      {id: 1, icon: 'http://pic.qqtn.com/up/2017-11/2017110215093888510.jpg', desc: '我好饿1，好饿'},
+      {id: 2, icon: 'http://pic.qqtn.com/up/2017-11/2017110215093888510.jpg', desc: '我好饿2，好饿'},
+      {id: 3, icon: 'http://pic.qqtn.com/up/2017-11/2017110215093888510.jpg', desc: '我好饿3，好饿'},
+      {id: 4, icon: 'http://pic.qqtn.com/up/2017-11/2017110215093888510.jpg', desc: '我好饿4，好饿'},
     ]
   },
   {
@@ -29,14 +48,13 @@ export const daily = [
     day: 20,
     dateRange: '2019-5-4~2019-6-8',
     title: '第二期直采,2019年2月17日 16:26:07',
-    video: 'http://pn01lr3h0.bkt.clouddn.com/tvc-v2_30097df.mp4',
+    video: 'http://pn01lr3h0.bkt.clouddn.com/eed39ae9805a34a922862eb773e6d9fa.mp4',
+    cover: 'http://pn01lr3h0.bkt.clouddn.com/eed39ae9805a34a922862eb773e6d9fa.mp4?vframe/jpg/offset/6',
     messages: [
-      '1111',
-      '22222',
-      '333333',
-      '4444444',
-      '55555555',
-      '666666666',
+      {id: 1, icon: 'http://pic.qqtn.com/up/2017-11/2017110215093888510.jpg', desc: '我好饿1，好饿'},
+      {id: 2, icon: 'http://pic.qqtn.com/up/2017-11/2017110215093888510.jpg', desc: '我好饿2，好饿'},
+      {id: 3, icon: 'http://pic.qqtn.com/up/2017-11/2017110215093888510.jpg', desc: '我好饿3，好饿'},
+      {id: 4, icon: 'http://pic.qqtn.com/up/2017-11/2017110215093888510.jpg', desc: '我好饿4，好饿'},
     ]
   },
   {
@@ -49,14 +67,13 @@ export const daily = [
     day: 20,
     dateRange: '2019-5-4~2019-6-8',
     title: '第一期直采,2019年2月17日 16:26:07',
-    video: 'http://pn01lr3h0.bkt.clouddn.com/tvc-v2_30097df.mp4',
+    video: 'http://pn01lr3h0.bkt.clouddn.com/67ddeee3d71ff5e1da63d35cb476a8dc.mp4',
+    cover: 'http://pn01lr3h0.bkt.clouddn.com/67ddeee3d71ff5e1da63d35cb476a8dc.mp4?vframe/jpg/offset/6',
     messages: [
-      '1111',
-      '22222',
-      '333333',
-      '4444444',
-      '55555555',
-      '666666666',
+      {id: 1, icon: 'http://pic.qqtn.com/up/2017-11/2017110215093888510.jpg', desc: '我好饿1，好饿'},
+      {id: 2, icon: 'http://pic.qqtn.com/up/2017-11/2017110215093888510.jpg', desc: '我好饿2，好饿'},
+      {id: 3, icon: 'http://pic.qqtn.com/up/2017-11/2017110215093888510.jpg', desc: '我好饿3，好饿'},
+      {id: 4, icon: 'http://pic.qqtn.com/up/2017-11/2017110215093888510.jpg', desc: '我好饿4，好饿'},
     ]
   },
 ];
--- pages_news/live_broadcast/live_broadcast.js
+++ pages_news/live_broadcast/live_broadcast.js
@@ -4,8 +4,47 @@ Page({
   data: {
     contentPaddingTop: getApp().globalData.contentPaddingTop,
     daily,
+    current: 0,
   },
   onLoad() {
+  },
+  nextSource(e) {
+    const { current } = e.detail;
+    this.setData({
+      current
+    });
+    if (this.videoContext) {
+      this.videoContext.stop();
+      this.videoContext.pause();
+    }
+    if (daily[current].video) {
+      this.videoContext = wx.createVideoContext(`videoContext${current}`);
+      this.videoContext.play();
+    }
+  },
+  tapLike(e) {
+    wx.showToast({
+      title: '点赞',
+      icon: 'none'
+    });
+  },
+  tapShare(e) {
+    wx.showToast({
+      title: '分享',
+      icon: 'none'
+    });
+  },
+  tapSuggest(e) {
+    wx.showToast({
+      title: '推荐商品',
+      icon: 'none'
+    });
+  },
+  messageChange(e) {
+    const {source } = e.detail;
+    if (source) {
+      return;
+    }
   },
   navigateBack() {
     wx.navigateBack();
