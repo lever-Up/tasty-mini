bbc619fe4d10b79d250e1d9702071ea505fe0de8
singingBoy <737708900@qq.com>
2019-02-21 18:34:52 +0800

    2019年2月21日 18:34:51

--- page_others/vip_room/vip_room.js
+++ page_others/vip_room/vip_room.js
@@ -1,6 +1,9 @@
+import {rpxToPx} from "../../utils/navbar_tool";
+
 Page({
   data:{
     contentPaddingTop: getApp().globalData.contentPaddingTop,
+    rpx140px: rpxToPx(140),
   },
   noneTap() {},
   navigateBack() {
