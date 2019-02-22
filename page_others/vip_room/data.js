c39fec45a28a7f27232c3cad9b8f3931f982005e
singingBoy <737708900@qq.com>
2019-02-22 18:20:34 +0800

    2019年2月22日 18:20:33

---
+++ page_others/vip_room/data.js
@@ -0,0 +1,4 @@
+export const banners = [
+  {id: 1, image: 'http://plsi1usd8.bkt.clouddn.com/chihuo_banner.jpg'},
+  {id: 2, image: 'http://plsi1usd8.bkt.clouddn.com/ad1.jpg'},
+];
--- page_others/vip_room/vip_room.js
+++ page_others/vip_room/vip_room.js
@@ -1,11 +1,17 @@
 import {rpxToPx} from "../../utils/navbar_tool";
+import {banners} from 'data';
 
 Page({
   data:{
     contentPaddingTop: getApp().globalData.contentPaddingTop,
     rpx140px: rpxToPx(140),
+    banners,
+    currentBannerIndex: 1,
   },
-  noneTap() {},
+  handleSwipeChange(e) {
+    const { current } = e.detail;
+    this.setData({ currentBannerIndex: current + 1 });
+  },
   navigateBack() {
     wx.navigateBack();
   },
