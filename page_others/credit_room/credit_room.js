8e5b4280369cfe387cb958f229d98fb2ae5c1744
wangdesheng <737708900@qq.com>
2019-02-23 17:34:57 +0800

    积分兑换

--- page_others/credit_room/credit_room.js
+++ page_others/credit_room/credit_room.js
@@ -1,5 +1,44 @@
+import {products} from './data';
+
 Page({
-  data:{},
+  data:{
+    contentPaddingTop: getApp().globalData.contentPaddingTop,
+    productsLeft: products.filter((d, i) => !(i % 2)),
+    productsRight: products.filter((d, i) => i % 2),
+  },
+  onLoad() {
+    // nav-bar 样式监听
+    this.barObserver = wx.createIntersectionObserver(this);
+    this.barObserver
+      .relativeToViewport({top: -100})
+      .observe('.credit-room', (res) => {
+        const { bottom } = res.intersectionRect;
+        if (bottom > 0) {
+          const barStyle = {
+            frontColor: '#ffffff',
+            backgroundColor: '#000000',
+          };
+          wx.setNavigationBarColor(barStyle);
+          this.setData({
+            navBarBackground: 'transparent',
+            navBarColor: barStyle.frontColor,
+          })
+        } else {
+          const barStyle = {
+            frontColor: '#000000',
+            backgroundColor: '#ffffff',
+          };
+          wx.setNavigationBarColor(barStyle);
+          this.setData({
+            navBarBackground: barStyle.backgroundColor,
+            navBarColor: barStyle.frontColor,
+          })
+        }
+      })
+  },
+  onUnload() {
+    if (this.barObserver) this.barObserver.disconnect();
+  },
   navigateBack() {
     wx.navigateBack();
   },
---
+++ page_others/credit_room/data.js
@@ -0,0 +1,44 @@
+export const products = [
+  {
+    id: 1,
+    title: '时尚背包',
+    image: 'http://yahwo.cn/5931367dN3ab70429.jpg',
+    score: 400
+  },
+  {
+    id: 2,
+    title: '蓝牙音响',
+    image: 'http://yahwo.cn/5720a974N7000560e.jpg',
+    score: 100
+  },
+  {
+    id: 3,
+    title: '电动牙刷',
+    image: 'http://yahwo.cn/59ef4791Ne4e0e441.jpg',
+    score: 200
+  },
+  {
+    id: 4,
+    title: '小米手环',
+    image: 'http://yahwo.cn/51e8b2599e48097b.png',
+    score: 300
+  },
+  {
+    id: 5,
+    title: '智能电饭煲',
+    image: 'http://yahwo.cn/5b55468cN0cecae9c.jpg',
+    score: 800
+  },
+  {
+    id: 6,
+    title: '蓝牙运动耳机',
+    image: 'http://yahwo.cn/5ac32a2eN3b616876.png',
+    score: 250
+  },
+  {
+    id: 7,
+    title: '热水壶',
+    image: 'http://yahwo.cn/fb2fff45a281aec4.jpg',
+    score: 150
+  },
+];
