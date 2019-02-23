9d70e2a558e7ebc32d6921ab4c5d20af988d92fe
wangdesheng <737708900@qq.com>
2019-02-23 16:45:00 +0800

    共享会员页

--- page_others/vip_room/vip_room.js
+++ page_others/vip_room/vip_room.js
@@ -8,6 +8,41 @@ Page({
     banners,
     currentBannerIndex: 1,
   },
+  onLoad() {
+    // nav-bar 样式监听
+    this.barObserver = wx.createIntersectionObserver(this);
+    this.barObserver
+      .relativeToViewport({top: -100})
+      .observe('.vip-room', (res) => {
+        const { bottom } = res.intersectionRect;
+        if (bottom > 0) {
+          const barStyle = {
+            frontColor: '#ffffff',
+            backgroundColor: '#000000',
+          };
+          wx.setNavigationBarColor(barStyle);
+          this.setData({
+            navBarBackground: 'transparent',
+            navBarColor: barStyle.frontColor,
+            fixedBottom: true,
+          })
+        } else {
+          const barStyle = {
+            frontColor: '#000000',
+            backgroundColor: '#ffffff',
+          };
+          wx.setNavigationBarColor(barStyle);
+          this.setData({
+            navBarBackground: barStyle.backgroundColor,
+            navBarColor: barStyle.frontColor,
+            fixedBottom: false,
+          })
+        }
+      })
+  },
+  onUnload() {
+    if (this.barObserver) this.barObserver.disconnect();
+  },
   handleSwipeChange(e) {
     const { current } = e.detail;
     this.setData({ currentBannerIndex: current + 1 });
